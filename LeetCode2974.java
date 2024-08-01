297a68b70a04c883db8aee5d547e8d43106866ed
Shouvanik21 <143490804+Shouvanik21@users.noreply.github.com>
2024-08-01 22:03:08 +0530

    LeetCode 2974.Minimum Number Game

---
+++ LeetCode2974.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+
+public class LeetCode2974 {
+    public int[] numberGame(int[] nums) {
+        Arrays.sort(nums);
+
+        for(int i = 0; i < nums.length; i += 2) {
+            int tmp = nums[i];
+            nums[i] = nums[i + 1];
+            nums[i + 1] = tmp;
+        }
+
+        return nums;
+    }
+}
