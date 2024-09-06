5cdbb622f9d72e24a05aee20507679f051415c0b
Shouvanik21 <143490804+Shouvanik21@users.noreply.github.com>
2024-09-06 22:18:12 +0530

    LeetCode 367.Valid Perfect Square

---
+++ LeetCode367.java
@@ -0,0 +1,18 @@
+public class LeetCode367 {
+    public boolean isPerfectSquare(int num) {
+        long low=0,high=num;
+        while(low<=high){
+            long mid=low+(high-low)/2;
+            if(mid*mid==num){
+                return true;
+            }
+            else if(mid*mid<num){
+                low=mid+1;
+            }
+            else{
+                high=mid-1;
+            }
+        }
+        return false;
+    }
+}
