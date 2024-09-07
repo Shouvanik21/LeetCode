6d611b1ed42d6c0080e56ad91dc0c72576a849ab
Shouvanik21 <143490804+Shouvanik21@users.noreply.github.com>
2024-09-07 23:33:40 +0530

    LeetCode 905.Sort Array By Parity

---
+++ LeetCode905.java
@@ -0,0 +1,22 @@
+public class LeetCode905 {
+    public int[] sortArrayByParity(int[] nums) {
+        int i=0,j=1;
+        while(j<nums.length){
+            if(nums[i]%2!=0 && nums[j]%2==0){
+                int temp=nums[i];
+                nums[i]=nums[j];
+                nums[j]=temp;
+                i++;
+                j++;
+            }
+            else if(nums[i]%2!=0 && nums[j]%2!=0){
+                j++;
+            }
+            else{
+                i++;
+                j++;
+            }
+        }
+        return nums;
+    }
+}
