3558620ef17fee171f1455741bea2a562892bf58
Shouvanik21 <143490804+Shouvanik21@users.noreply.github.com>
2024-09-26 22:03:16 +0530

    LeetCode 162.Find Peak Element

---
+++ LeetCode162.java
@@ -0,0 +1,24 @@
+public class LeetCode162 {
+    public int findPeakElement(int[] nums) {
+        if(nums.length==1){
+            return 0;
+        }
+        else if(nums[0]>nums[1]){
+            return 0;
+        }
+        else if(nums[nums.length-1]>nums[nums.length-2]){
+            return nums.length-1;
+        }
+        else{
+            int peak=Integer.MIN_VALUE;
+            for(int i=1;i<nums.length-1;i++){
+                if(nums[i]>nums[i+1] && nums[i]>nums[i-1]){
+                    if(nums[i]>peak){
+                        peak=i;
+                    }
+                }
+            }
+            return peak;
+        }
+    }
+}
