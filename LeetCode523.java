ee34d4b4b7c9bf6f7a9fc91c62cf2900c87b650a
Shouvanik21 <143490804+Shouvanik21@users.noreply.github.com>
2024-10-20 22:31:44 +0530

    LeetCode 523.Continuous Subarray Sum

--- LeetCode523.java
+++ LeetCode523.java
@@ -1,36 +1,23 @@
-public boolean checkSubarraySum (int[] nums, int k) {
+public class LeetCode523 {
+    public boolean checkSubarraySum(int[] nums, int k) {
+        HashMap<Integer,Integer> map = new HashMap<>();//the first one stores the remainder and the second one stores the index value
+        map.put(0,-1);
+        int sum=0;
 
-       HashMap<Integer, Integer> map = new HashMap<>(); //the first one stores the remainder and the second one stores the index value
+        for(int i=0;i<nums.length;i++){
+            sum+=nums[i];//calculating the sum of subarray
+            int rem=sum%k;//calculating the remainder
 
-map.put(0, -1);
+            if(map.containsKey(rem)){//if remmainder is found
+                if(i-map.get(rem)>=2){//count the val of remainder occurs for how many no
+                    return true;//if condn satisfy return true
+                }
+            }
+            else{
+                map.put(rem,i);//if not found add remainder and the index
+            }
+        }
 
-int sum=0;
-
-for (int i=0;i<nums.length; i++){
-
-sum+ nums[i]; //calculating the sum of subarray
-
-int rem-sum%k; //calculating the remainder
-
-if(map.containsKey(rem)){//if remmainder is found
-
-if(i-map.get(rem)>=2){//count the val of remainder occurs for how many no
-
-return true; //if condn satisfy return true
-
+        return false;//if condn not satisfy return false
+    }
 }
-
-}
-
-else{
-
-map.put(rem, i); //if not found add remainder and the index.
-
-}
-
-}
-
-return false; //if condn not satisfy return false
-
-} 
-
