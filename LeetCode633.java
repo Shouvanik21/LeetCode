6d80be1bcb817b5f0f56c8a78ea8c7ef5417f579
Shouvanik21 <143490804+Shouvanik21@users.noreply.github.com>
2024-10-23 23:51:56 +0530

    LeetCode 633.Sum of Square Numbers

---
+++ LeetCode633.java
@@ -0,0 +1,17 @@
+public class LeetCode633 {
+    public boolean judgeSquareSum(int c) {
+        long left=0,right=(long)Math.sqrt(c);
+        while(left<=right){
+            if(left*left+right*right==c){
+                return true;
+            }
+            else if(left*left+right*right>c){
+                right--;
+            }
+            else{
+                left++;
+            }
+        }
+        return false;
+    }
+}
