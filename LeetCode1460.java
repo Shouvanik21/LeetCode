6efd964f4bd6841084f852357c550b32a1204bd6
Shouvanik21 <143490804+Shouvanik21@users.noreply.github.com>
2024-11-15 23:23:30 +0530

    LeetCode 1460.Make Two Arrays Equal by Reversing Subarrays

---
+++ LeetCode1460.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+
+public class LeetCode1460 {
+    public boolean canBeEqual(int[] target, int[] arr) {
+        int[] cnt1 = new int[1001];
+        int[] cnt2 = new int[1001];
+        for (int v : target) {
+            ++cnt1[v];
+        }
+        for (int v : arr) {
+            ++cnt2[v];
+        }
+        return Arrays.equals(cnt1, cnt2);
+    }
+}
