f23278bae883f3e929ee0a3cea855e4e8fe7e1f8
Shouvanik21 <143490804+Shouvanik21@users.noreply.github.com>
2024-11-22 20:35:44 +0530

    LeetCode 2221.Find Triangular Sum of an Array

---
+++ LeetCode2221.java
@@ -0,0 +1,16 @@
+public class LeetCode2221 {
+    public int triangularSum(int[] nums) {
+        return dfs(nums);
+    }
+
+    public int dfs(int[] nums){
+        if(nums.length == 1){
+            return nums[0];
+        }
+        int temp[] = new int[nums.length - 1];
+        for(int i = 0; i < nums.length-1; i++){
+            temp[i] = (nums[i] + nums[i+1]) % 10;
+        }
+        return dfs(temp);
+    }
+}
