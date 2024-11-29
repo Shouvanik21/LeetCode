2d80506cf7b7db9336d5684a70c2930c4c7c9952
Shouvanik21 <143490804+Shouvanik21@users.noreply.github.com>
2024-11-29 21:56:28 +0530

    LeetCode 326.Power of Three

---
+++ LeetCode326.java
@@ -0,0 +1,15 @@
+public class LeetCode326 {
+    public boolean isPowerOfThree(int n) {
+        if (n == 1) return true;
+        while(0 < n){
+            if (n % 3 != 0){
+                return false;
+            }
+            n = n/3;
+            if (n == 1){
+                return true;
+            }
+        }
+        return false;
+    }
+}
