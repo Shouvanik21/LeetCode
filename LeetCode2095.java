642558da8709b05eaac5fee1c6ed0d4b64070d81
Shouvanik21 <143490804+Shouvanik21@users.noreply.github.com>
2024-12-28 22:44:44 +0530

    LeetCode 2095.Delete the Middle Node of a Linked List

---
+++ LeetCode2095.java
@@ -0,0 +1,22 @@
+public class LeetCode2095 {
+    public ListNode deleteMiddle(ListNode head) {
+        if(head==null || head.next==null){
+            return null;
+        }
+
+        ListNode slow=head;
+        ListNode fast=head;
+        fast=head.next.next;
+
+        while(fast!=null && fast.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+        }
+
+        if(slow.next!=null){
+            slow.next=slow.next.next;
+        }
+
+        return head;
+    }
+}
