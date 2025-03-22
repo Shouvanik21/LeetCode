ee286364dbf0d2e5de72c8d298c06445fdd219a9
Shouvanik21 <143490804+Shouvanik21@users.noreply.github.com>
2025-03-22 23:36:51 +0530

    LeetCode 342.Power of Four

---
+++ LeetCode342.java
@@ -0,0 +1,15 @@
+public class LeetCode342 {
+    public boolean isPowerOfFour(int n) {
+        for (int i = 0; i <= 15; i++) {
+            int powerOfFour = (int) Math.pow(4, i);
+            
+            if (powerOfFour == n)
+                return true;
+            
+            if (powerOfFour > n)
+                return false;
+        }
+        
+        return false;
+    }
+}
