51002089a0950fe3daca5f189da5cf7c7560349c
Shouvanik21 <143490804+Shouvanik21@users.noreply.github.com>
2025-05-16 23:03:38 +0530

    LeetCode 28.Find the Index of the First Occurrence in a String

---
+++ LeetCode28.java
@@ -0,0 +1,11 @@
+public class LeetCode28 {
+    public int strStr(String haystack, String needle) {
+        int m = haystack.length(), n = needle.length();
+        for (int i = 0; i <= m - n; i++) {
+            if (haystack.substring(i, i + n).equals(needle)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
