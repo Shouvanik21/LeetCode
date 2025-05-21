10942ea5bc84414d05c4de16c8f6efede47b9e3a
Shouvanik21 <143490804+Shouvanik21@users.noreply.github.com>
2025-05-21 23:40:34 +0530

    LeetCode 1910.Remove All Occurrences of a Substring

---
+++ LeetCode1910.java
@@ -0,0 +1,9 @@
+public class LeetCode1910 {
+    public String removeOccurrences(String s, String part) {
+        while(s.contains(part)){
+            int i = s.indexOf(part);
+            s = s.substring(0,i)+s.substring(i+part.length());
+        }
+        return s;
+    }
+}
